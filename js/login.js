ac4f09abcdd46ed82c4ac887100bfa966a87c2ee
PedroSilvDev-0 <silvsrhyan@gmail.com>
2023-11-17 16:04:11 -0300

    Bloqueando usuarios nao logados de acessar

--- js/login.js
+++ js/login.js
@@ -1,3 +1,9 @@
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        window.location.href = "home.html"
+    }
+})
+
 function onChangeEmail() {
     toggleButtonsDisable()
     toggleEmailErrors()
